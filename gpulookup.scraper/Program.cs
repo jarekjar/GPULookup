421e5b6c100624572613cb25671ff798953c610c
Jared Kjar <jaredkjar@gmail.com>
2018-03-10 15:17:17 -0800

    wip

--- gpulookup.scraper/Program.cs
+++ gpulookup.scraper/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using IronWebScraper;
 
 namespace GpuLookup.Scraper
 {
@@ -14,7 +16,11 @@ namespace GpuLookup.Scraper
             var page1 = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48/";
             WebClient webClient = new WebClient();
             var result = webClient.DownloadString(page1);
-            Console.WriteLine(result);
+            string replaceWith = "";
+            string match = "<a\\s+(?:[^>]*?\\s+)?href=\"([^ \"]*)\"";
+            result = result.Replace("\r\n", replaceWith).Replace("\n", replaceWith).Replace("\r", replaceWith);
+            var image_link_match = Regex.Match(result, match).Value;
+            Console.WriteLine(image_link_match);
             Console.ReadLine();
         }
     }
