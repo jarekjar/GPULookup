69b2beb91e170f6e0eba2c023f711838473f2775
Jared Kjar <jaredkjar@gmail.com>
2018-03-10 15:44:08 -0800

    wip

--- gpulookup.scraper/Program.cs
+++ gpulookup.scraper/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using AngleSharp.Extensions;
 using AngleSharp.Parser.Html;
 
 namespace GpuLookup.Scraper
@@ -13,14 +14,28 @@ namespace GpuLookup.Scraper
     {
         static void Main(string[] args)
         {
+            var gpus = new List<GPU>();
+            GPU gpu = null;
             var parser = new HtmlParser();
             var page1 = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48/";
             WebClient webClient = new WebClient();
             var result = webClient.DownloadString(page1);
             var document = parser.Parse(result);
             Console.WriteLine("Serializing the (original) document:");
-            Console.WriteLine(document.DocumentElement.OuterHtml);
+            var pricesListItemsLinq = document.QuerySelectorAll(".item-brand");
+            foreach(var item in pricesListItemsLinq)
+            {
+                gpu = new GPU();
+                string img = item.InnerHtml;
+                gpu.ImgTag = Regex.Replace(img, @"\s+", " ");
+                gpus.Add(gpu);
+            }
+            Console.WriteLine(gpus);
             Console.ReadLine();
         }
+    }
+    class GPU
+    {
+        public string ImgTag { get; set; }
     }
 }
