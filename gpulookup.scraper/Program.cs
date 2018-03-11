bcd2ab21ab4be48b1212dde1b8630dfac2adbb08
Jared Kjar <jaredkjar@gmail.com>
2018-03-10 16:42:30 -0800

    wip

--- gpulookup.scraper/Program.cs
+++ gpulookup.scraper/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -19,18 +21,42 @@ namespace GpuLookup.Scraper
             var parser = new HtmlParser();
             var page1 = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48/";
             WebClient webClient = new WebClient();
-            var result = webClient.DownloadString(page1);
-            var document = parser.Parse(result);
-            Console.WriteLine("Serializing the (original) document:");
-            var pricesListItemsLinq = document.QuerySelectorAll(".item-brand");
-            foreach(var item in pricesListItemsLinq)
+            var page = 1;
+            while (page < 60)
             {
-                gpu = new GPU();
-                string img = item.InnerHtml;
-                gpu.ImgTag = Regex.Replace(img, @"\s+", " ");
-                gpus.Add(gpu);
+                string result = null;
+                if (page == 1)
+                {
+                    result = webClient.DownloadString(page1);
+                }
+                else
+                {
+                    result = webClient.DownloadString(page1 + "Page-" + page);
+                }
+                var document = parser.Parse(result);
+                var items = document.QuerySelectorAll(".item-title");
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.TextContent);
+                    using (SqlConnection connection = new SqlConnection("Data Source=localhost;Initial Catalog=GpuLookup;Integrated Security = true"))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            string sql = "INSERT INTO TEST_TABLE(name) VALUES(@name)";
+                            SqlCommand cmd = new SqlCommand(sql, connection);
+                            cmd.Parameters.Add("@name", SqlDbType.NVarChar).Value = item.TextContent;
+                            cmd.CommandType = CommandType.Text;
+                            cmd.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+                page++;
             }
-            Console.WriteLine(gpus);
             Console.ReadLine();
         }
     }
