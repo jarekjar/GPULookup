97e0b4064c9a992304b8c0688e1195c1e1505c83
Jared Kjar <jaredkjar@gmail.com>
2018-03-12 12:00:18 -0700

    wip

--- GpuLookup.Parser/Program.cs
+++ GpuLookup.Parser/Program.cs
@@ -17,6 +17,8 @@ namespace GpuLookup.Parser
 
         private static void ParseTitles(string connectionString)
         {
+            var gpu = new GPU();
+            List<GPU> gpus = null;
             string queryString = "SELECT * FROM TEST_TABLE";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -25,11 +27,42 @@ namespace GpuLookup.Parser
                 SqlDataReader reader = command.ExecuteReader();
                 while (reader.Read())
                 {
-                    string title = reader.GetString(0);
+                    if(gpus == null)
+                    {
+                        gpus = new List<GPU>();
+                    }
+                    string[] titleWords = reader.GetString(0).Split();
+                    gpu.Manufacturer = titleWords[0];
+                    titleWords[0] = "";
+                    gpu.ChipName = String.Join("", titleWords);
+                    gpus.Add(gpu);
                 }
                 reader.Close();
             }
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
+            //InsertGpus(gpus, connectionString);
         }
+
+        //private static void InsertGpus(List<GPU> gpus, string conString)
+        //{
+        //    using (SqlConnection connection = new SqlConnection(conString))
+        //    {
+        //        try
+        //        {
+        //            connection.Open();
+        //            string sql = "INSERT INTO TEST_TABLE(name) VALUES(@name)";
+        //            SqlCommand cmd = new SqlCommand(sql, connection);
+        //            cmd.Parameters.Add("@name", SqlDbType.NVarChar).Value = item.TextContent;
+        //            cmd.CommandType = CommandType.Text;
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+        //    }
+        //}
     }
     class GPU
     {
