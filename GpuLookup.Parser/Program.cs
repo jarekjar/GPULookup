169191a7bcd300860790498e4ff83e9e8e22b860
Jared Kjar <jaredkjar@gmail.com>
2018-03-12 11:48:28 -0700

    wip

--- GpuLookup.Parser/Program.cs
+++ GpuLookup.Parser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,31 @@ namespace GpuLookup.Parser
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string str = "Data Source=localhost;Initial Catalog=GpuLookup;Integrated Security=True";
+            ParseTitles(str);
         }
+
+        private static void ParseTitles(string connectionString)
+        {
+            string queryString = "SELECT * FROM TEST_TABLE";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string title = reader.GetString(0);
+                }
+                reader.Close();
+            }
+        }
+    }
+    class GPU
+    {
+        public string Manufacturer { get; set; }
+        public string ChipName { get; set; }
     }
 }
