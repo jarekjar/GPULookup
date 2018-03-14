c0557ac0c0e269740822ae609ea56911d4c19f52
Jared Kjar <jaredkjar@gmail.com>
2018-03-13 19:04:04 -0700

    wip

--- GpuLookup.Web/src/index.js
+++ GpuLookup.Web/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyNavbar from './Navbar';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyNavbar />, document.getElementById('root'));
 registerServiceWorker();
