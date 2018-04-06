327ae9a3e6ad3ba2e36faa6a56b8178cbb664e90
Jared Kjar <jaredkjar@gmail.com>
2018-04-05 17:00:39 -0700

    wip

--- GpuLookup.Scraper/Program.cs
+++ GpuLookup.Scraper/Program.cs
@@ -49,7 +49,8 @@ namespace GpuLookup.Scraper
                 NeweggScrape(),
                 AmazonScrape(),
                 MicrocenterScrape(),
-                BestBuyScrape()
+                BestBuyScrape(),
+                //SomeOtherWebsiteScrape()
             );
             Console.WriteLine("Filtering the SQL Table.....");
             FilterDuplicates();
